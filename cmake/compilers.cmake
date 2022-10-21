38250e9cf2f6f6a36987c7824d3ebcb910daf65d
Michael Hirsch <scivision@users.noreply.github.com>
2022-10-21 10:36:56 -0400

    always try to run compiler MPI_F08 test

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -2,21 +2,19 @@ include(CheckFortranSourceCompiles)
 
 find_package(MPI REQUIRED COMPONENTS C Fortran)
 
-set(CMAKE_REQUIRED_LIBRARIES MPI::MPI_Fortran)
+set(CMAKE_REQUIRED_INCLUDES ${MPI_Fortran_INCLUDE_DIRS})
+set(CMAKE_REQUIRED_LIBRARIES ${MPI_Fortran_LIBRARIES})
 
-if(MPI_Fortran_HAVE_F08_MODULE)
-  check_fortran_source_compiles(
-  [=[
-  program test
-  use mpi_f08, only : mpi_comm_rank, mpi_real, mpi_comm_world, mpi_init, mpi_finalize
-  implicit none
-  call mpi_init
-  call mpi_finalize
-  end program
-  ]=]
-  MPI_Fortran_F08_MODULE_WORKS
-  SRC_EXT f90
-  )
-endif()
-
-set(CMAKE_REQUIRED_LIBRARIES)
+# sometimes factory FindMPI.cmake doesn't define this
+check_fortran_source_compiles(
+[=[
+program test
+use mpi_f08, only : mpi_comm_rank, mpi_real, mpi_comm_world, mpi_init, mpi_finalize
+implicit none
+call mpi_init
+call mpi_finalize
+end program
+]=]
+MPI_Fortran_HAVE_F08_MODULE
+SRC_EXT f90
+)
