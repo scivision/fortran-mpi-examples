8d7f13276689e159cf9b2addeb5659c56ec61cf9
scivision <scivision@users.noreply.github.com>
2023-12-13 18:52:16 -0500

    stdlib

--- src/basic.c
+++ src/basic.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <mpi.h>
 
 int main(int argc, char **argv)
 {
-   int ierr;
-
    printf("going to init MPI\n");
 
-   ierr = MPI_Init(&argc,&argv);
-   if(ierr != 0) {
+   if(MPI_Init(&argc,&argv)) {
       perror("could not init MPI\n");
-      return 1;
+      return EXIT_FAILURE;
    }
    printf("MPI Init OK\n");
 
-   ierr = MPI_Finalize();
-   if(ierr != 0) {
+   if(MPI_Finalize()) {
       perror("could not close MPI\n");
-      return 1;
+      return EXIT_FAILURE;
    }
 
    printf("MPI closed\n");
 
-   return 0;
+   return EXIT_SUCCESS;
 }
