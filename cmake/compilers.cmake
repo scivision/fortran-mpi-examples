94bc6d82e7a9eec30a22002a38fd86d1c3ea1e3c
scivision <scivision@users.noreply.github.com>
2023-12-13 18:36:26 -0500

    add additional MPI-3 configure diagnostics

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -4,10 +4,28 @@ set(MPI_DETERMINE_LIBRARY_VERSION true)
 find_package(MPI REQUIRED COMPONENTS C Fortran)
 message(STATUS "MPI Library Version: ${MPI_C_LIBRARY_VERSION_STRING}")
 
+find_file(mpi_f08_mod NAMES mpi_f08.mod
+NO_DEFAULT_PATH
+HINTS ${MPI_Fortran_INCLUDE_DIRS}
+)
+
+message(STATUS "${MPI_Fortran_LIBRARY_VERSION_STRING}")
+message(STATUS "MPI libs: ${MPI_Fortran_LIBRARIES}")
+message(STATUS "MPI include: ${MPI_Fortran_INCLUDE_DIRS}")
+message(STATUS "MPI_f08 module: ${mpi_f08_mod}")
+message(STATUS "MPI compile flags: ${MPI_Fortran_COMPILER_FLAGS}")
+message(STATUS "MPI link flags: ${MPI_Fortran_LINK_FLAGS}")
+
+if(NOT mpi_f08_mod)
+  message(FATAL_ERROR "Fortran MPI ${MPI_Fortran_VERSION} doesn't have MPI-3 Fortran mpi_f08.mod, searched using ${MPI_Fortran_INCLUDE_DIRS}")
+endif()
+
 include(${CMAKE_CURRENT_LIST_DIR}/openmpi.cmake)
 
 set(CMAKE_REQUIRED_INCLUDES ${MPI_Fortran_INCLUDE_DIRS})
 set(CMAKE_REQUIRED_LIBRARIES ${MPI_Fortran_LIBRARIES})
+
+
 
 # sometimes factory FindMPI.cmake doesn't define this
 check_source_compiles(Fortran
