9cebb0e6d01b2a7c0af6c95ce7c6715ba12e00d6
scivision <scivision@users.noreply.github.com>
2023-12-19 18:46:13 -0500

    FindMPI: cray workaround [skip ci]

--- cmake/mpi.cmake
+++ cmake/mpi.cmake
@@ -4,6 +4,27 @@ set(MPI_DETERMINE_LIBRARY_VERSION true)
 find_package(MPI REQUIRED COMPONENTS C Fortran)
 message(STATUS "MPI Library Version: ${MPI_C_LIBRARY_VERSION_STRING}")
 
+# Cray FindMPI.cmake has a bug where the plain CMake variables aren't defined, only imported target is
+# as a workaround for the many versions of CMake where this is so, we populate them ourselves.
+
+if(NOT MPI_Fortran_INCLUDE_DIRS)
+  get_property(MPI_Fortran_INCLUDE_DIRS TARGET MPI::MPI_Fortran PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+  message(STATUS "workaround: MPI_Fortran_INCLUDE_DIRS: ${MPI_Fortran_INCLUDE_DIRS}")
+endif()
+if(NOT MPI_C_INCLUDE_DIRS)
+  get_property(MPI_C_INCLUDE_DIRS TARGET MPI::MPI_C PROPERTY INTERFACE_INCLUDE_DIRECTORIES)
+  message(STATUS "workaround: MPI_C_INCLUDE_DIRS: ${MPI_C_INCLUDE_DIRS}")
+endif()
+if(NOT MPI_Fortran_COMPILE_OPTIONS)
+  get_property(MPI_Fortran_COMPILE_OPTIONS TARGET MPI::MPI_Fortran PROPERTY INTERFACE_COMPILE_OPTIONS)
+endif()
+if(NOT MPI_Fortran_COMPILE_DEFINITIONS)
+  get_property(MPI_Fortran_COMPILE_DEFINITIONS TARGET MPI::MPI_Fortran PROPERTY INTERFACE_COMPILE_DEFINITIONS)
+endif()
+if(NOT MPI_Fortran_LINK_FLAGS)
+  get_property(MPI_Fortran_LINK_FLAGS TARGET MPI::MPI_Fortran PROPERTY INTERFACE_LINK_OPTIONS)
+endif()
+
 find_file(mpi_f08_mod NAMES mpi_f08.mod
 NO_DEFAULT_PATH
 HINTS ${MPI_Fortran_INCLUDE_DIRS}
@@ -17,8 +38,8 @@ message(STATUS "MPI_Fortran_MODULE_DIR: ${MPI_Fortran_MODULE_DIR}")
 message(STATUS "MPI_Fortran_INCLUDE_DIRS: ${MPI_Fortran_INCLUDE_DIRS}")
 
 message(STATUS "MPI_f08 module: ${mpi_f08_mod}")
-message(STATUS "MPI compile flags: ${MPI_Fortran_COMPILER_FLAGS}")
-message(STATUS "MPI link flags: ${MPI_Fortran_LINK_FLAGS}")
+message(STATUS "MPI_Fortran_COMPILE_OPTIONS: ${MPI_Fortran_COMPILE_OPTIONS}")
+message(STATUS "MPI_Fortran_LINK_FLAGS: ${MPI_Fortran_LINK_FLAGS}")
 
 if(NOT mpi_f08_mod)
   message(WARNING "Fortran MPI ${MPI_Fortran_VERSION} doesn't have MPI-3 Fortran mpi_f08.mod, searched using ${MPI_Fortran_INCLUDE_DIRS}")
