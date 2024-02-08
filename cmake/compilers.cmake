d4cec5586b13e38889e694a51823083c4c618d4d
scivision <scivision@users.noreply.github.com>
2024-02-08 09:42:14 -0500

    correct compiler options:

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,3 +1,9 @@
 if(NOT MSVC)
-  add_compile_options(-Wall)
+  add_compile_options($<$<COMPILE_LANGUAGE:C>:-Wall>)
+endif()
+
+if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+  add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-Wall>)
+elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
+  add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-warn>)
 endif()
