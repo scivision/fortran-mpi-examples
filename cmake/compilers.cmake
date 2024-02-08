132092797eed94377ecf759cd0c6749b6643d681
scivision <scivision@users.noreply.github.com>
2024-02-08 09:49:22 -0500

    more intel options

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -5,5 +5,9 @@ endif()
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-Wall>)
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
-  add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-warn>)
+  add_compile_options(
+  "$<$<COMPILE_LANGUAGE:Fortran>:-warn>"
+  "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-check;-traceback>"
+  "$<$<CONFIG:Debug>:-Rno-debug-disables-optimization>"
+  )
 endif()
