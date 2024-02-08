cee233dee563452eedd22466924fd77b73af8bd7
scivision <scivision@users.noreply.github.com>
2024-02-08 10:22:49 -0500

    compiler debug options

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,9 +1,19 @@
-if(NOT MSVC)
+if(CMAKE_C_COMPILER_ID MATCHES "GNU|Clang|Intel")
   add_compile_options($<$<COMPILE_LANGUAGE:C>:-Wall>)
+endif()
+
+if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
+  add_compile_options(
+  "$<$<AND:$<COMPILE_LANGUAGE:C>,$<CONFIG:Debug,RelWithDebInfo>>:-Wextra>"
+  )
 endif()
 
 if(CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
   add_compile_options($<$<COMPILE_LANGUAGE:Fortran>:-Wall>)
+  add_compile_options(
+    "$<$<AND:$<COMPILE_LANGUAGE:Fortran>,$<CONFIG:Debug,RelWithDebInfo>>:-Wextra;-fcheck=all;-Werror=array-bounds>"
+    )
+
 elseif(CMAKE_Fortran_COMPILER_ID MATCHES "^Intel")
   add_compile_options(
   "$<$<COMPILE_LANGUAGE:Fortran>:-warn>"
